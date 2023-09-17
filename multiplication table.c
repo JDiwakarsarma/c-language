ea856469f966a27c922a3f92a0952306db895ea2
JDiwakarsarma <96375253+JDiwakarsarma@users.noreply.github.com>
2023-09-17 11:41:20 +0530

    Add files via upload

---
+++ multiplication table.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    printf("the multiplication table");
+    for(int i=1; i<11 ; i++){
+
+
+
+        printf("i   %d *10=  %d \n" , i , i*10);
+    }
+    return 0;
+
+}
