61c50ceca16c48e8924fca1747af3043e3307064
JDiwakarsarma <96375253+JDiwakarsarma@users.noreply.github.com>
2023-09-17 11:43:37 +0530

    Add files via upload

---
+++ multiplication table in reverse order.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    printf("the multiplication table");
+    for(int i=10; i ; i--){
+
+
+
+        printf("i   %d *10=  %d \n" , i , i*10);
+    }
+    return 0;
+
+}
