8665ded54e565d59b4f40d8ef1fb200fff879a36
JDiwakarsarma <96375253+JDiwakarsarma@users.noreply.github.com>
2023-09-25 22:32:46 +0530

    Add files via upload

---
+++ function goodmorning , goodafternoon , goodnight.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+void goodmorning();
+void goodafternoon();
+void goodnight();
+
+
+int main(){
+    printf("hello diwakar \n");
+    goodmorning();
+    goodafternoon();
+    goodnight();
+    printf("your decorum is made\n");
+
+    
+    return 0;
+}
+
+
+void goodmorning(){
+
+    printf("goodmorning sir\n");
+
+}
+void goodafternoon(){
+
+    printf("goodafternoon sir\n");
+
+}
+void goodnight(){
+    printf("goodnight sir \n");
+
+}
