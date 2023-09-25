fd7102c51037d2c224a6b5000579b54e5eed2579
JDiwakarsarma <96375253+JDiwakarsarma@users.noreply.github.com>
2023-09-25 22:20:44 +0530

    Add files via upload

---
+++ FUNCTION.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void display();
+
+
+int main(){
+    int a;
+    printf("hello diwakar\n");
+    display();
+    printf("display function finished its work\n");
+
+
+    
+    return 0;
+}
+void display(){
+
+
+    printf("this is my world \n");
+
+}
