e27f5f8e66ac405daec423f9b73b2f6ddbaa00fd
JDiwakarsarma <96375253+JDiwakarsarma@users.noreply.github.com>
2023-10-06 23:44:54 +0530

    Add files via upload

---
+++ array .c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main(){
+
+
+int marks[4];
+printf("enter the marks of 1st person");
+scanf("%d",&marks[0]);
+printf("enter the marks of 2nd person");
+scanf("%d",&marks[1]);
+printf("enter the marks of 3rd person");
+scanf("%d",&marks[2]);
+printf("enter the marks of 4th person");
+scanf("%d",&marks[3]);
+
+printf("the entered marks of 1st  2nd  3rd  and 4th persons are %d,%d,%d,%d",marks[0],marks[1],marks[2],marks[3]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+
+}
